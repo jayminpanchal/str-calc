f6d1a4a5878d4d3fd87f275eafaad891cb1a499f
Jaymin Panchal <jayminpanchal10@gmail.com>
2024-08-04 20:24:23 +0530

    Added inital form with input and button

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { ChangeEvent, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [expression, setExpression] = useState("");
+  const [result, setResult] = useState("");
+
+  const onChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setExpression(e.target.value);
+  };
+
+  const onCalculate = () => {};
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="p-8 flex flex-1 items-center justify-center">
+      <div className="flex space-x-2 items-center">
+        <input
+          className="h-10 border border-slate-500 rounded flex flex-1 w-64 px-4"
+          type="text"
+          data-testid="input-expression"
+          onChange={onChange}
+        />
+        <button
+          onClick={onCalculate}
+          className="bg-slate-950 text-white px-4 py-2 rounded hover:bg-slate-700 cursor-pointer disabled:bg-slate-400 disabled:cursor-not-allowed"
+          type="button"
+          data-testid="btn-calc"
+          disabled={!expression}
         >
-          Learn React
-        </a>
-      </header>
+          Calculate
+        </button>
+      </div>
+      {result && <p data-testid="text-result">{result}</p>}
     </div>
   );
 }
